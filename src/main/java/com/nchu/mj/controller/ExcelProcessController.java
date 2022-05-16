0c578a53a1beb889c393a6601d85e0919821e190
chenjianhui.cjh <chenjianhui.cjh@alibaba-inc.com>
2022-05-16 19:21:05 +0800

    feat: 完成视图层改造

--- src/main/java/com/nchu/mj/controller/ExcelProcessController.java
+++ src/main/java/com/nchu/mj/controller/ExcelProcessController.java
@@ -2,16 +2,19 @@ package com.nchu.mj.controller;
 
 
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 
 import com.nchu.mj.service.StudentExcelProcess;
+import org.apache.poi.util.IOUtils;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,7 +28,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class ExcelProcessController {
 
     @PostMapping("/excel/process")
-    public ResponseEntity<Resource> hello(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<Resource> excelProcess(@RequestParam("file") MultipartFile file) {
         try {
             byte[] bytes = StudentExcelProcess.build().process(file.getBytes());
             HttpHeaders headers = new HttpHeaders();
--- src/main/java/com/nchu/mj/service/StudentExcelProcess.java
+++ src/main/java/com/nchu/mj/service/StudentExcelProcess.java
@@ -3,7 +3,6 @@ package com.nchu.mj.service;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
