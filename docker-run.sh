f44f8a95aff25a3314aae199f156875abe99db77
chenjianhui.cjh <chenjianhui.cjh@alibaba-inc.com>
2022-05-17 16:04:08 +0800

    build: docker 账户配置

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,6 @@
-docker run -p 3333:3333 -e "JAVA_OPTS=-Ddebug -Xmx128m" nchu-tools-app --server.port=3333
+docker run --name nchu-tools-app \
+    --restart always \
+    -d \
+    -p 3333:3333 \
+    -e "JAVA_OPTS=-Ddebug -Xmx128m" \
+    ayayating1031/nchu-tools-app --server.port=3333
